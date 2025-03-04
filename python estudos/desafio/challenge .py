8ed59e53f3e5bb3ca811f2c10174dd789b4c9c7f
Lucas Magalhães <103212436+Olderc4t@users.noreply.github.com>
2025-03-04 09:58:28 -0300

    Update challenge .py

--- python estudos/desafio/challenge .py
+++ python estudos/desafio/challenge .py
@@ -5,14 +5,12 @@ Crie um jogo simples onde o computador escolhe um número aleatório entre 1 e 1
 """
 import random
 numero_secreto = random.randint(1, 100)
-
 numero_tentativas = 5
 
 while numero_tentativas > 0:
     numero_user = int(input(f'diga um numero de 1 a 100! Mas cuidado pois so tem {numero_tentativas} tentativas'))
 
     numero_tentativas -=1
-    
 
     if numero_user == numero_secreto :
         print('Parabens vc acertou!')
@@ -24,4 +22,3 @@ while numero_tentativas > 0:
 
     if numero_tentativas == 0:
             print(f"não foi dessa vez o numero era {numero_secreto}, mas tente novamente!")
-
